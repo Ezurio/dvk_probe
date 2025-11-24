e9e72c77ee4ea7733fe358281360709e3d6418ef
Ryan Erickson <ryan.erickson@ezurio.com>
2025-11-24 14:01:37 -0600

    Customize app
    Remove sample references.
    Config both UARTs for use with flow control.
    Enable debugging.
    Change main to only change settings on the targeted UART instead
    of looping through all devices.
    Set RTS back high when USB UART is closed.
    
    PROD-46827
    
    Signed-off-by: Ryan Erickson <ryan.erickson@ezurio.com>

---
+++ include/app_usbd.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA.
+ * Copyright 2025 Ezurio
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef APP_USBD_H
+#define APP_USBD_H
+
+#include <stdint.h>
+#include <zephyr/usb/usbd.h>
+
+/*
+ * This function configures and initializes a
+ * USB device. It configures the device context, default string descriptors,
+ * USB device configuration, registers any available class instances, and
+ * finally initializes USB device.
+ *
+ * It returns the configured and initialized USB device context on success,
+ * otherwise it returns NULL.
+ */
+struct usbd_context *app_usbd_init_device(usbd_msg_cb_t msg_cb);
+
+/*
+ * This function is similar to app_usbd_init_device(), but does not
+ * initialize the device. It allows the application to set additional features,
+ * such as additional descriptors.
+ */
+struct usbd_context *app_usbd_setup_device(usbd_msg_cb_t msg_cb);
+
+#endif /* APP_USBD_H */
--- src/main.c
+++ src/main.c
@@ -1,34 +1,39 @@
 /*
  * Copyright 2025 Google LLC
+ * Copyright 2025 Ezurio
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <sample_usbd.h>
+#include "app_usbd.h"
 
 #include <stdio.h>
 #include <string.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/uart/uart_bridge.h>
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/uart.h>
 
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/usb/usbd.h>
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(cdc_acm_bridge, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(dvk_probe, LOG_LEVEL_INF);
 
-const struct device *const uart_dev = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
-
-static struct usbd_context *sample_usbd;
+static struct usbd_context *app_usbd;
 
 #define DEVICE_DT_GET_COMMA(node_id) DEVICE_DT_GET(node_id),
 
 const struct device *uart_bridges[] = {
-	DT_FOREACH_STATUS_OKAY(zephyr_uart_bridge, DEVICE_DT_GET_COMMA)
-};
+	DT_FOREACH_STATUS_OKAY(zephyr_uart_bridge, DEVICE_DT_GET_COMMA)};
 
-static void sample_msg_cb(struct usbd_context *const ctx, const struct usbd_msg *msg)
+static void usbd_msg_cb(struct usbd_context *const ctx, const struct usbd_msg *msg)
 {
+	uint32_t line_ctrl_status;
+	uint8_t dev_idx;
+	const struct device *uart_dev = NULL;
+	int ret;
+	struct uart_config peer_cfg;
+
 	LOG_INF("USBD message: %s", usbd_msg_type_string(msg->type));
 
 	if (usbd_can_detect_vbus(ctx)) {
@@ -47,11 +52,47 @@ static void sample_msg_cb(struct usbd_context *const ctx, const struct usbd_msg 
 
 	if (msg->type == USBD_MSG_CDC_ACM_LINE_CODING ||
 	    msg->type == USBD_MSG_CDC_ACM_CONTROL_LINE_STATE) {
-		for (uint8_t i = 0; i < ARRAY_SIZE(uart_bridges); i++) {
-			/* update all bridges, non valid combinations are
-			 * skipped automatically.
-			 */
-			uart_bridge_settings_update(msg->dev, uart_bridges[i]);
+		for (dev_idx = 0; dev_idx < ARRAY_SIZE(uart_bridges); dev_idx++) {
+			/* Find the matching device */
+			uart_dev = uart_bridge_get_peer(msg->dev, uart_bridges[dev_idx]);
+			if (uart_dev != NULL) {
+				break;
+			}
+		}
+
+		if (uart_dev == NULL) {
+			LOG_DBG("No matching UART bridge for %s", msg->dev->name);
+			return;
+		}
+
+		/* Get the current UART configuration of the USB CDC ACM device */
+		ret = uart_config_get(msg->dev, &peer_cfg);
+		if (ret != 0) {
+			LOG_ERR("Failed to get UART config: %d", ret);
+			return;
+		}
+
+		/* Check if the USB CDC ACM device is ready for operation */
+		ret = uart_line_ctrl_get(msg->dev, UART_LINE_CTRL_DTR, &line_ctrl_status);
+		if (ret == 0) {
+			if (line_ctrl_status) {
+				LOG_INF("DTR set: enable UART bridge %s", uart_dev->name);
+				uart_bridge_settings_update(msg->dev, uart_bridges[dev_idx]);
+				peer_cfg.flow_ctrl = UART_CFG_FLOW_CTRL_RTS_CTS;
+			} else {
+				LOG_INF("DTR cleared: disable UART bridge %s", uart_dev->name);
+				/* This sets RTS back high when the USB UART is closed */
+				peer_cfg.flow_ctrl = UART_CFG_FLOW_CTRL_NONE;
+			}
+
+			ret = uart_configure(uart_dev, &peer_cfg);
+			if (ret) {
+				LOG_ERR("%s: failed to set the uart config: %d", uart_dev->name,
+					ret);
+				return;
+			}
+		} else {
+			LOG_ERR("Failed to get DTR status: %d", ret);
 		}
 	}
 }
@@ -60,14 +101,14 @@ int main(void)
 {
 	int err;
 
-	sample_usbd = sample_usbd_init_device(sample_msg_cb);
-	if (sample_usbd == NULL) {
+	app_usbd = app_usbd_init_device(usbd_msg_cb);
+	if (app_usbd == NULL) {
 		LOG_ERR("Failed to initialize USB device");
 		return -ENODEV;
 	}
 
-	if (!usbd_can_detect_vbus(sample_usbd)) {
-		err = usbd_enable(sample_usbd);
+	if (!usbd_can_detect_vbus(app_usbd)) {
+		err = usbd_enable(app_usbd);
 		if (err) {
 			LOG_ERR("Failed to enable device support");
 			return err;
